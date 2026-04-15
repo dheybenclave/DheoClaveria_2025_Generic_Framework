eb7044aca76a5c31c70aa4c7f9903dedf8e7f43a
Dheo Claveria <dheybenclave@gmail.com>
2026-04-15 14:24:00 +0800

    added dismissCookieBanner

--- src/test/java/pages/CommonPage.java
+++ src/test/java/pages/CommonPage.java
@@ -8,7 +8,6 @@ public class CommonPage extends UIInteractions {
     public WebElementFacade LBL_FIELD(String name) {return $("//*[contains(text(),'"+name+"')]");}
     public WebElementFacade LBL_FIELD_WITH_PARENT_SELECTOR(String parentSelector, String name) {return $( parentSelector+"//*[contains(text(),'"+name+"')]");}
     public WebElementFacade PAGE_CONTROL_LABEL(String labelText) { return $(String.format("//*[contains(text(),'%s')]", labelText));}
-    public WebElementFacade BANNER_COOKIES() { return $(String.format("//*[contains(text(),'%s')]", labelText));}
 
 
 }
